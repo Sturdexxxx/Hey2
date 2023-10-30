d21cbb4571b3de713f61cbd0cb6840f21135493f
Sturdexxxx <134261890+Sturdexxxx@users.noreply.github.com>
2023-10-30 15:29:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append()
+ADMINS.append(1943966786)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
